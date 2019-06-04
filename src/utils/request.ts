d24332b1b06b8c5a9d6c8fef521298d81aecddb5
zhp <354416479@qq.com>
2019-06-04 21:46:33 +0800

    remove useless file

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,17 +1,17 @@
 import axios from 'axios'
+import store from 'store'
 // import router from '@/router'
 
 // import sysConfig from '@/utils/sysConfig'
 
 const service = axios.create({
-  baseURL: 'http://haopengzh.cn:3001',
+  // baseURL: 'http://haopengzh.cn:3001',
   // baseURL: 'http://192.168.108.125',
   timeout: 30 * 1000
 })
 
 service.interceptors.request.use(config => {
-  let token = window.localStorage.getItem('haopengzh_token')
-  console.log(token)
+  let token = store.get('haopengzh_token')
   config.headers['Authorization'] ='Bearer '+token;
   return config
 }, error => {
@@ -30,7 +30,6 @@ service.interceptors.response.use(
     } catch (e) {
       msg = error.message
     }
-    console.log(msg)
     return Promise.reject(error)
   })
 
